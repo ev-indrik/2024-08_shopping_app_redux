f35a372e1b6af5020b64a15e9179c6895765b412
unknown <delfinmur@gmail.com>
2024-06-17 14:27:05 +0300

    UserContext implemented in Sign In and Sign Up, Authentication components, first sign out layout logic added

--- src/components/sign-up-form/sign-up.component.component.jsx
+++ src/components/sign-up-form/sign-up.component.component.jsx
@@ -1,14 +1,16 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 
 import {
   createAuthUserWithEmailAndPassword,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
+import { UserContext } from "../../contexts/user.context";
+
 import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
 
 import "./sign-up-form.styles.scss";
-import Button from "../button/button.component";
 
 const defaultFormFields = {
   displayName: "",
@@ -20,6 +22,8 @@ const defaultFormFields = {
 const SignUpForm = () => {
   const [formFields, setFormFields] = useState(defaultFormFields);
   const { displayName, email, password, confirmPassword } = formFields;
+
+  const { setCurrentUser } = useContext(UserContext);
 
   const resetFormFields = () => {
     setFormFields(defaultFormFields);
@@ -34,16 +38,15 @@ const SignUpForm = () => {
     }
 
     try {
-      const userCredential = await createAuthUserWithEmailAndPassword(
+      const { user } = await createAuthUserWithEmailAndPassword(
         email,
         password
       );
 
-      if (userCredential) {
-        const { user } = userCredential;
-        await createUserDocumentFromAuth(user, { displayName });
-        resetFormFields();
-      }
+      setCurrentUser(user);
+
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
     } catch (error) {
       if (error.code === "auth/email-already-in-use") {
         alert("Cannot create user, email already in use");
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -11,17 +11,21 @@ const Navigation = () => {
   console.log(currentUser);
   return (
     <>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <div className={"navigation"}>
+        <Link className={"logo-container"} to="/">
           <img src={CrwnLogo} alt="main-logo" />
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/">
-            Shop
+        <div className={"nav-links-container"}>
+          <Link className={"nav-link"} to={"/"}>
+            {"Shop"}
           </Link>
-          <Link className="nav-link" to="/auth">
-            Sign In
-          </Link>
+          {currentUser ? (
+            <span className={"nav-link"}>{"Sign Out"}</span>
+          ) : (
+            <Link className={"nav-link"} to={"/auth"}>
+              {"Sign In"}
+            </Link>
+          )}
         </div>
       </div>
       <Outlet />
