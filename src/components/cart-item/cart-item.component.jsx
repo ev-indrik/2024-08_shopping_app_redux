41bd0a92d2653160b242b05a710a165570fd5b69
unknown <delfinmur@gmail.com>
2024-06-20 22:44:52 +0300

    adding to cart functionality created and added to components via CartContext

---
+++ src/components/cart-item/cart-item.component.jsx
@@ -0,0 +1,12 @@
+const CartItem = ({ cartItem }) => {
+  const { name, quantity } = cartItem;
+
+  return (
+    <div className="cart-item-container">
+      <h2>{name}</h2>
+      <span>{quantity}</span>
+    </div>
+  );
+};
+
+export default CartItem;
