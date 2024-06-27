e215df3887f56c4ff8182ea802bcefd70c40441e
unknown <delfinmur@gmail.com>
2024-06-27 15:57:52 +0300

    checkout component modified, styles added

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -2,30 +2,49 @@ import { useContext } from "react";
 
 import { CartContext } from "../../contexts/cart.context";
 
+import "./checkout.styles.scss";
+
 const Checkout = () => {
   const { cartItems, addItemToCart, removeItemFromCart } =
     useContext(CartContext);
 
   return (
-    <div>
-      <h2>{"I am Checkout"}</h2>
-      <div>
-        {cartItems.map((cartItem) => {
-          const { id, name, quantity } = cartItem;
-          return (
-            <div key={id}>
-              <h2>{name}</h2>
-              <span>{quantity}</span>
-              <br />
-              <span onClick={() => removeItemFromCart(cartItem)}>
-                decrement
-              </span>
-              <br />
-              <span onClick={() => addItemToCart(cartItem)}>increment</span>
-            </div>
-          );
-        })}
+    <div className="checkout-container">
+      <div className="checkout-header">
+        <div className="header-block">
+          <span>{"Product"}</span>
+        </div>
+
+        <div className="header-block">
+          <span>{"Desciption"}</span>
+        </div>
+        <div className="header-block">
+          <span>{"Quantity"}</span>
+        </div>
+        <div className="header-block">
+          <span>{"Price"}</span>
+        </div>
+        <div className="header-block">
+          <span>{"Remove"}</span>
+        </div>
       </div>
+
+      {cartItems.map((cartItem) => {
+        const { id, name, quantity, price } = cartItem;
+        return (
+          <div key={id}>
+            <h2>{name}</h2>
+            <span>{quantity}</span>
+            <br />
+            <span onClick={() => removeItemFromCart(cartItem)}>
+              {"decrement"}
+            </span>
+            <br />
+            <span onClick={() => addItemToCart(cartItem)}>{"increment"}</span>
+          </div>
+        );
+      })}
+      <span className="total">{"Total: 0"}</span>
     </div>
   );
 };
