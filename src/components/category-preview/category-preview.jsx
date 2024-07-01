c536afd50150b69d6f55b01b350e2857657d475a
unknown <delfinmur@gmail.com>
2024-07-01 20:09:57 +0300

    CategoryPreview component created

---
+++ src/components/category-preview/category-preview.jsx
@@ -0,0 +1,20 @@
+import ProductCard from "../product-card/product-card.component";
+
+const CategoryPreview = ({ title, products }) => {
+  return (
+    <div className="category-preview-container">
+      <h2>
+        <span className="title">{title.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, inx) => inx < 4)
+          .map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryPreview;
