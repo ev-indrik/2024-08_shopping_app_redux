f288459ea0a0244f976c49ce5a0a85beaba2eda7
unknown <delfinmur@gmail.com>
2024-08-07 18:38:23 +0300

    store is set up, root-reducer created

--- src/main.jsx
+++ src/main.jsx
@@ -8,18 +8,23 @@ import { UserProvider } from "./contexts/user.context.jsx";
 import { CategoriesProvider } from "./contexts/categories.context.jsx";
 import { CartProvider } from "./contexts/cart.context.jsx";
 
+import { Provider } from "react-redux";
+import { store } from "./store/store.js";
+
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <UserProvider>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <UserProvider>
+          <CategoriesProvider>
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
