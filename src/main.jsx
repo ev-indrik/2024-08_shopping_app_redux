8e72147ef70e964a36a723412b7071146533c93d
unknown <delfinmur@gmail.com>
2024-08-09 20:26:06 +0300

    Store's category redux is set up

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 
-import { CategoriesProvider } from "./contexts/categories.context.jsx";
 import { CartProvider } from "./contexts/cart.context.jsx";
 
 import { Provider } from "react-redux";
@@ -16,11 +15,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
-        <CategoriesProvider>
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,8 +1,9 @@
-import { Fragment, useContext, useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
+
+import { useSelector } from "react-redux";
+import { selectCategoriesMap } from "../../store/categories/category.selector";
 
 import { useParams } from "react-router-dom";
-
-import { CategoriesContext } from "../../contexts/categories.context";
 
 import ProductCard from "../../components/product-card/product-card.component";
 
@@ -10,7 +11,7 @@ import "./category.styles.scss";
 
 const Category = () => {
   const { category } = useParams(); // дістати параметр "category" з об'єкта, повернутого useParams
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category] || []);
 
   useEffect(() => {
