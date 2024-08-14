fc43f8acd7cc9e55a1497a6b6dbe80cbaadec292
unknown <delfinmur@gmail.com>
2024-08-14 13:44:08 +0300

    Redux-Presist (local storage) added and set up

--- src/main.jsx
+++ src/main.jsx
@@ -2,19 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import App from "./App.jsx";
+import { PersistGate } from "redux-persist/integration/react";
+
+import App from "./App";
 
 import { Provider } from "react-redux";
-import { store } from "./store/store.js";
+import { store, persistor } from "./store/store.js";
 
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
