2410ea6732c85fa7a47a2c78f552bc1f46f01789
unknown <delfinmur@gmail.com>
2024-08-16 15:12:45 +0300

    redux-saga added, thunk removed

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
-import { thunk } from "redux-thunk";
+// import { thunk } from "redux-thunk";
+
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
 
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
@@ -10,14 +13,16 @@ import { rootReducer } from "./root-reducer";
 const persistConfig = {
   key: root,
   storage,
-  blacklist: ["cart"],
+  whitelist: ["cart"],
 };
+
+const sagaMiddleware = createSagaMiddleware();
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [
   process.env.NODE_ENV !== "production" && logger,
-  thunk,
+  sagaMiddleware,
 ].filter(Boolean);
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
@@ -28,5 +33,7 @@ export const store = createStore(
   undefined,
   composedEnhancers
 );
+
+sagaMiddleware.run(rootSaga);
 
 export const persistor = persistStore(store);
