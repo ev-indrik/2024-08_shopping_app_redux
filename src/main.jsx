f810863c9de31f0eb1c8de4e7f393f11ac10b6c4
unknown <delfinmur@gmail.com>
2024-08-21 18:50:25 +0300

    general setup for redux-toolkit

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
-import { PersistGate } from "redux-persist/integration/react";
+// import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 
 import { Provider } from "react-redux";
-import { store, persistor } from "./store/store.js";
+import { store } from "./store/store.js";
 
 import "./index.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
-      </PersistGate>
+      {/* <PersistGate persistor={persistor}> */}
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
