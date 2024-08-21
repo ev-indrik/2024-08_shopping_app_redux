1e5c2722b2ee5b722336537830fc9e593589fdf1
unknown <delfinmur@gmail.com>
2024-08-21 17:48:55 +0300

    returning to certain code-state before redux asunc libraries were added

--- src/store/store.js
+++ src/store/store.js
@@ -1,21 +1,29 @@
 import { compose, createStore, applyMiddleware } from "redux";
-import logger from "redux-logger";
-
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
 const persistConfig = {
-  key: root,
+  key: "root",
   storage,
   blacklist: ["user"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
