94a7337716942b8f16ac4328f71727303b913be3
unknown <delfinmur@gmail.com>
2024-08-24 18:55:53 +0300

    categories reducers transformed to redux-toolkit

--- src/store/categories/category.action.js
+++
@@ -1,6 +0,0 @@
-import { createAction } from "../../utils/reducer/reducer.utils";
-
-import { CATEGOIRES_ACTION_TYPES } from "./category.types";
-
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGOIRES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
